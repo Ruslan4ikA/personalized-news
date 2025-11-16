ba8230bdd2004de6c93c8a5a8194d4b88c090494
Ruslan4ikA <ruslanr26@mail.ru>
2025-11-16 20:45:59 +0500

    добавил .env

--- core/settings.py
+++ core/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9nm3pesqf4ua*q7qo-(mfshq%3l5lmg**-)(hpnu*@$@5&2nlw'
+SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-for-local-dev-only')  # fallback только для разработки
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
